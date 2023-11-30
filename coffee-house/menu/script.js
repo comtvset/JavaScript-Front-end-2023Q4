67875f50e247531cf0b20c7ad20e807be970dc49
Vadzim Tsikhaniuk <comtvset@gmail.com>
2023-11-30 20:32:20 +0100

    Coffee-House. Week-2 - is done (Thu, Nov 30, 2023 08:32:20 PM)

--- coffee-house/menu/script.js
+++ coffee-house/menu/script.js
@@ -1,37 +1,47 @@
 console.log(
     `
-1. Checking validation of pages: +18 ✅
-- The layout for both pages is validated and error-free according to the W3C Validator (https://validator.w3.org/): +12 (6 points per page) Valid markup of checked page corresponds to the message "Document checking completed. No errors or warnings to show." In this case, we assign the full points for the checked page (+6). If there are warnings but no errors, we assign half of the points (+3) for the checked page ✅
-- Favicon is added to each page: +2 ✅
-- Each page has only one <h1> element: +2 ✅
-- The URL of the menu page differs from the URL of the home page (e.g. your-site.com for the home page and your-site.com/menu for the menu page): +2 ✅
+1. The layout of the pages aligns the design at a screen width of 1440px: +14 ✅
+- <header> block on each page: +2 ✅
+- Enjoy block on home page: +2 ✅
+- Favourites Coffee block on home page: +2 ✅
+- About block on home page: +2 ✅
+- Mobile App block on home page: +2 ✅
+- Menu block on menu page: +2 ✅
+- <footer> block on each page: +2 ✅
 
-2. The layout matches the design: +40 ✅
-- <header> block on each page: +4 ✅
-- Enjoy block on home page: +6 ✅
-- Favourites Coffee block on home page: +6 ✅
-- About block on home page: +6 ✅
-- Mobile App block on home page: +6 ✅
-- Menu block on menu page: +6 ✅
-- <footer> block on each page: +6 ✅
+2. The layout of the pages aligns the design at a screen width of 768px: +14 ✅
+- <header> block on each page: +2 ✅
+- Enjoy block on home page: +2 ✅
+- Favourites Coffee block on home page: +2 ✅
+- About block on home page: +2 ✅
+- Mobile App block on home page: +2 ✅
+- Menu block on menu page: +2 ✅
+- <footer> block on each page: +2 ✅
 
-3. CSS Requirements: +10 ✅
-- For positioning images in About block on home page and products in Menu block on menu page used Flexbox or Grid Layout: +4 ✅
-- When scaling the browser page (<100%) or increasing the page width (>1440px), the layout of both pages is centered rather than shifted to the side and not stretched across the entire width: +4 ✅
-- The background color Body stretches across the entire width of the page: +2 ✅
+3. The layout of the pages aligns the design at a screen width of 380px: +14 ✅
+- <header> block on each page: +2 ✅
+- Enjoy block on home page: +2 ✅
+- Favourites Coffee block on home page: +2 ✅
+- About block on home page: +2 ✅
+- Mobile App block on home page: +2 ✅
+- Menu block on menu page: +2 ✅
+- <footer> block on each page: +2 ✅
 
-4. Interactivity: +32
-- Navigation elements (except Contact us) lead to corresponding blocks on home page (anchor links): +4 ✅
-- Contact us in navigation panel links to the <footer> block on its own page (anchor link): +2 ✅
-- Smooth scrolling with anchor links: +2 ✅
-- When clicking on the Menu buttons in header and Enjoy block on home page, it navigates to the menu page: +2 ✅
-- The Menu button in header on menu page is non-interactive: +2 ✅
-- When clicking on the Logo in header, it navigates to the home page: +2 ✅
-- The active Coffee button in Menu block of Menu page is non-interactive: +2 ✅
-- Each Coffee-card in the Menu section of the Menu page is interactive when hovering over any area of the card: +4 ✅
-- In the <footer> block, clicking on the link with phone number (all area including icon) should initiate a phone call: +2 ✅
-- In the <footer> block, clicking on the link with the address (all area including icon) should open a new browser tab with Google Maps displaying any location of your choice: +2 ✅
-- Interactivity of links and buttons is implemented according to Figma layout. Interactivity includes not only changing cursor's appearance, for example, using the cursor: pointer property, but also the use of other visual effects, such as changing the background color or font color, following the Styleguide in Figma layout: +4 ✅
-- Mandatory requirement for interactivity: smooth change in the appearance of an element on hover and click, without affecting adjacent elements: +4 ✅
+4. There is no horizontal scrollbar at all screen width up to 380px inclusive. All page content remains as per the design: it is not cropped, removed, or shifted to the side: +20 ✅
+- home page: no horizontal scroll bar between 1440px and 768px widths: +5 ✅
+- home page: no horizontal scroll bar between 768px and 380px widths: +5 ✅
+- menu page: no horizontal scroll bar between 1440px and 768px widths: +5 ✅
+- menu page: no horizontal scroll bar between 768px and 380px widths: +5 ✅
+
+5. During smooth resizing of the browser window from 1440px to 380px, the layout occupies the full width of the window (including specified margins), elements adjust their sizes and positions appropriately without full scaling, no elements overlap, and images maintain their correct aspect ratios: +8 ✅
+- On home page: +4 ✅
+- On menu page: +4 ✅
+
+6. At screen widths of 768px and below, the menu and navigation buttons in the header are concealed on both pages, and a burger menu icon is displayed: +4 ✅
+(Note: Activation of the burger menu icon is not evaluated at this stage.)
+
+7. Hover effects are active on desktop devices (as per the Desktop device type in DevTools) and are disabled for mobile devices (as per the Mobile device type in DevTools) on both pages: +4 ✅
+
+8. The layout for both pages is validated and error-free according to the W3C Validator (https://validator.w3.org/): +12 ✅
     `
 );
