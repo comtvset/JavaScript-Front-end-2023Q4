9982963a6cfccd47c8fd88fdc3a86fbae3c5ac3e
Vadzim Tsikhaniuk <comtvset@gmail.com>
2024-02-23 14:57:33 +0100

    fix: from news.js to news.ts (Fri, Feb 23, 2024 02:57:33 PM)

---
+++ news-js/src/components/types/index.ts
@@ -0,0 +1,13 @@
+export default interface NewsData {
+    author: string;
+    content: string;
+    description: string;
+    publishedAt: string;
+    source: {
+        id: string;
+        name: string;
+    };
+    title: string;
+    url: string;
+    urlToImage: string;
+}
