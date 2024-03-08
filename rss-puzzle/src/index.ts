7738c42d77cbd1250dcc85acfea30083a32f30f9
Vadzim Tsikhaniuk <comtvset@gmail.com>
2024-03-08 15:48:53 +0100

    test

---
+++ rss-puzzle/src/index.ts
@@ -0,0 +1,10 @@
+import './style.css';
+
+const array = [1, 2, 4];
+
+for (let i = 0; i < array.length; i += 1) {
+  console.log('Hello World!');
+}
+
+const num: number[] = [1, 2];
+console.log(num);
---
+++ rss-puzzle/webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.ts',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/i,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.html$/,
+        use: 'html-loader',
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+    new ESLintPlugin({
+      extensions: ['ts', 'tsx'],
+      exclude: ['/node_modules/', '/dist/'],
+    }),
+  ],
+};
