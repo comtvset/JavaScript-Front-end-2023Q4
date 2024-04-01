09601f3727edf13138bb43e8f1cbb06258a3cdaf
Vadzim Tsikhaniuk <comtvset@gmail.com>
2024-04-02 00:11:34 +0200

    feat: RSS-AR-02 alert for review (Tue, Apr 02, 2024 12:11:34 AM)

--- async-race/src/index.ts
+++ async-race/src/index.ts
@@ -39,3 +39,5 @@ toWinner.addButton('to Winner', () => {
 
 openGarage();
 toGarage.setDisabled();
+
+alert('Привет. Если есть возможность проверить в последний день cros-checka буду очень благодарен!');
